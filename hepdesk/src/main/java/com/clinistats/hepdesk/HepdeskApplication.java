aba30c762718c59124d5c6fd423db22e4803eadb
lukman.dalal <lukman.d@clinistats.com>
2022-11-14 13:17:07 +0530

    inital commit

--- hepdesk/src/main/java/com/clinistats/hepdesk/HepdeskApplication.java
+++ hepdesk/src/main/java/com/clinistats/hepdesk/HepdeskApplication.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class HepdeskApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HepdeskApplication.class, args);
 	}
